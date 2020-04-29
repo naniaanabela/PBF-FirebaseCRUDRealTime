e43793755066a7e8c8e48b79a45f9cb1bcf93b6f
Nurcahya Nania Anabela <nania.anabela22@gmail.com>
2020-04-30 06:06:23 +0700

    fixed blogpost

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyA65PluX4kOI-41PTcEfuHGG4fwipuinVo",
-  authDomain: "nurcahya-firebaselogin.firebaseapp.com",
-  databaseURL: "https://nurcahya-firebaselogin.firebaseio.com",
-  projectId: "nurcahya-firebaselogin",
-  storageBucket: "nurcahya-firebaselogin.appspot.com",
-  messagingSenderId: "101096352187",
-  appId: "1:101096352187:web:6bc0e57a93271c68bdfc9e"
+  apiKey: "AIzaSyCMiO7FD-eri3XYly62ankl4eMtCcHPXCI",
+  authDomain: "reacrt-firebasecrud.firebaseapp.com",
+  databaseURL: "https://reacrt-firebasecrud.firebaseio.com",
+  projectId: "reacrt-firebasecrud",
+  storageBucket: "reacrt-firebasecrud.appspot.com",
+  messagingSenderId: "576724536554",
+  appId: "1:576724536554:web:817309429d5017459b5b93"
 };
 
   export default firebaseConfig;
